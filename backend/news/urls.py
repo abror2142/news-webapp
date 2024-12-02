f6da49e34917994315944fecf355e02d43e15110
Abrorbek Ismoilov <abror2142@gmail.com>
2024-12-02 15:06:41 +0500

    Creating News API Endpoint

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from .views import all_categories, latest_posts
+
 
 urlpatterns = [
-    
+    path('latest/', latest_posts, name='latest_posts'),
+    path('category/', all_categories, name='all_categories'),    
 ]
--- backend/news/views.py
+++ backend/news/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from .models import Post, Category
+from .serializers import PostSerializer, CategorySerializer
 
 
+@api_view(['GET'])
+def latest_posts(request: Request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
+
+@api_view(['GET'])
+def all_categories(request: Request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
